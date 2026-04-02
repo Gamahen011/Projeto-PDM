f361fb0c7481745893be18bdf3bef9c4f75810d6
beatrizrgvs <beatriz.rg@aluno.ifsc.edu.br>
2026-04-02 14:30:48 -0300

    Versão inicial do projeto

---
+++ src/app/helloWorld/helloWorld.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'app-hello-world',
+  templateUrl: 'helloWorld.html',
+  styleUrls: ['helloWorld.scss']
+
+})
+export class HelloWorldComponent {
+
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { HelloWorldComponent } from '../helloWorld/helloWorld';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [HelloWorldComponent, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
   constructor() {}
